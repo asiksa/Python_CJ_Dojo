2fa8e9e31b341c5997b58709107408706648d8a5
Joanna Pruszkowska <39525035+asiksa@users.noreply.github.com>
2020-06-05 15:17:36 +0100

    Update list_comprehensions.py

--- list_comprehensions.py
+++ list_comprehensions.py
@@ -1,1 +1,20 @@
+#[1, 4, 9, 16, 25, 36]
+#[36,25,16,9,4,1]
 
+a=[]
+for x in range(1,7):
+  a.append(x**2)
+print(a)
+
+#[1, 4, 9, 16, 25, 36]
+
+a=[x**2 for x in range(1,7)]
+print(sorted(a, reverse=True))
+#[36, 25, 16, 9, 4, 1]
+
+b=[]
+for x in range(6,0,-1):
+  b.append(x**2)
+print(b)
+
+#[36, 25, 16, 9, 4, 1]
